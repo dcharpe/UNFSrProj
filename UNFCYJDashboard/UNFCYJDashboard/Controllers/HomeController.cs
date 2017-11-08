1929726320f4148ba813fb4b134d5be2d45a4101
dcharpe <n00618091@ospreys.unf.edu>
2017-11-08 18:03:36 -0500

    Upload Project

---
+++ UNFCYJDashboard/UNFCYJDashboard/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using UNFCYJDashboard.Models;
+
+namespace UNFCYJDashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        //instantiates an object from the database
+        CYJDashEntities dc = new CYJDashEntities();
+
+        //displays list of records in the database 
+        public ActionResult Index( )
+        {
+            //for every attribute in listed the following code will turn it into a list
+            var query = dc.Admins.ToList().Select(p => new Admin
+            {
+                //each attribute data that will be display
+                UserId = p.UserId,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                Email = p.Email,
+                Password = p.Password
+            });
+            //return a complete record to the index view
+            return View(query);
+        }
+
+       
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(Admin u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CYJDashEntities dc = new CYJDashEntities())
+                {
+                    dc.Admins.Add(u);
+                    dc.SaveChanges();
+                   
+                }
+                ModelState.Clear();
+                u = null;
+               // ViewBag.Message = u.FirstName + " " + u.LastName + " has successfully registered!";
+            }
+            return View(u);
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ UNFCYJDashboard/UNFCYJDashboard/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UNFCYJDashboard.Models;
+
+namespace UNFCYJDashboard.Controllers
+{
+    public class UserController : Controller
+    {
+
+        //instantiates an object from the database
+        CYJDashEntities dc = new CYJDashEntities();
+
+        //displays list of records in the database 
+        public ActionResult Index()
+        {
+            //for every attribute in listed the following code will turn it into a list
+            var query = dc.Admins.ToList().Select(p => new Admin
+            {
+                //each attribute data that will be display
+                UserId = p.UserId,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                Email = p.Email,
+                Password = p.Password
+            });
+            //return a complete record to the index view
+            return View(query);
+        }
+
+
+        // GET: User
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Admin u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CYJDashEntities dc = new CYJDashEntities())
+                {
+                    dc.Admins.Add(u);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    u = null;
+                    ViewBag.Message = u.FirstName + " " + u.LastName + " has successfully registered!";
+                }
+            }
+            return View(u);
+        }
+    }
+}
